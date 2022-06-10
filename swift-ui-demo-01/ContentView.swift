3e5bc5407838e05bfb0bd021312732b6a5bb3648
baobaomi900901 <baobaomi900901@icloud.com>
2022-06-10 11:06:24 +0800

    font-size

--- swift-ui-demo-01/ContentView.swift
+++ swift-ui-demo-01/ContentView.swift
@@ -26,17 +26,20 @@ struct ContentView: View {
                 // 容器大小
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20)
-            Text("SwiftUI for iOS 15")
+            Text("必抽福盒SwiftUI for iOS 15")
                 // 字体标签
-                .font(.largeTitle)
+//                .font(.largeTitle)
+                .font(.custom("FONT_NAME", size: 13*1.5))
                 // font-weight
                 .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
                 // 多行文本对齐
                 // .multilineTextAlignment(.leading)
-            Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+//                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text("Build an iOS app for iOS 15 wi th custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
@@ -49,19 +52,43 @@ struct ContentView: View {
 //        // 叠加计算的 padding
         .padding(.vertical, 20)
         .frame(height: 350)
-        .background(Color("Background"))
+//        // 背景颜色
+//        .background(Color("Background"))
+          // 背景材料 - Material
+        .background(.ultraThinMaterial)
         .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
 //        .shadow(radius: 20)
-        .shadow(color: Color("Shadow"), radius: 16, x: 0, y: 4)
+//        .shadow(color: Color.red.opacity(0.5), radius: 16, x: 0, y: 4)
+//        .shadow(color: Color(red: 0.5, green: 0.5, blue: 0.5,opacity: 0.5), radius: 16, x: 0, y: 4)
+        .shadow(color: Color("Shadow").opacity(0.5), radius: 16, x: 0, y: 4)
 //        // 阴影计算在内的 padding
         .padding(.horizontal, 20)
+        .background(
+            Image("Blob 1")
+                .offset(x:0 , y:-100)
+        )
+//        // 覆盖容器
+        .overlay(
+            Image("Illustration 5")
+                .resizable(resizingMode: .stretch)
+                // 纵横比
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -89)
+            
+        )
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-//            .padding()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 12 Pro Max")
+        // 可访问性
+//            .environment(\.sizeCategory, .accessibilityExtraExtraLarge)
+        
     }
 }
  
